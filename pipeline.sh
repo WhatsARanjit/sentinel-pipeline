e1af58bfeeb9268b5dc6e0e2b3eec601a6a02e71
WhatsARanjit <whatsaranjit@gmail.com>
2022-05-22 18:04:53 -0400

    Add check that tests exist for all policies

--- pipeline.sh
+++ pipeline.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+violations=()
+exitcode=0
+
+# Check to make sure every policy has a pass/fail test
+for i in $(ls -1 *.sentinel); do
+  policy=$(basename -s .sentinel $i)
+
+  if [[ ! -f "test/${policy}/pass.hcl" && ! -f "test/${policy}/fail.hcl" ]]; then
+    violations+=("$policy")
+    exitcode=1
+  fi
+done
+
+# Echo any violations
+for i in "${violations[@]}"; do
+  >&2 echo "${i}.sentinel does not have any pass/fail tests"
+done
+
+# Stop here if any violations
+if [[ $exitcode -ne 0 ]]; then
+  exit $exitcode
+fi
+
+# Run tests if all tests exist
 curl -o sentinel.zip -L "https://releases.hashicorp.com/sentinel/${SENTINEL_VERSION}/sentinel_${SENTINEL_VERSION}_linux_amd64.zip"
 unzip sentinel.zip
 chmod +x sentinel
